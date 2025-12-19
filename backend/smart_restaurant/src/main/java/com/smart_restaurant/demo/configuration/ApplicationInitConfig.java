ea494bb6db6c1331850853eae521e74ab9960d83
Do Ngoc Cuong <cuong01656499081@gmail.com>
2025-12-19 16:43:00 +0700

    fix application config

--- backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/configuration/ApplicationInitConfig.java
+++ backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/configuration/ApplicationInitConfig.java
@@ -38,18 +38,18 @@ public class ApplicationInitConfig {
         return args -> {
             if(accountRepository.findByUsername("admin").isEmpty()){
                 var roles = new ArrayList<Role>();
-                Optional<Role> role=roleRepository.findByName(Roles.TENANT_ADMIN.toString());
+                Optional<Role> role=roleRepository.findByName(Roles.SUPPER_ADMIN.toString());
                 if((role.isEmpty())){
                     Role adminRole = roleRepository.save(Role.builder()
-                            .name(Roles.TENANT_ADMIN.toString())
+                            .name(Roles.SUPPER_ADMIN.toString())
                             .build());
                     roles.add(adminRole);
                 }else{
                     roles.add(role.get());
                 }
                 Account account= Account.builder()
-                        .username("admin")
-                        .password(passwordEncoder.encode("admin"))
+                        .username("superadmin")
+                        .password(passwordEncoder.encode("superadmin"))
                         .isFirstActivity(false)
                         .isCustomer(false)
                         .isEmailVerify(true)
