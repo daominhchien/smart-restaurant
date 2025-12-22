fd271809553743bee05dbd6bff064f7104e75b4b
daominhchien <daominhchien2k4@gmail.com>
2025-12-22 21:03:53 +0700

    Create API Update, Delete, GetDetailModifierGroup

--- backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/mapper/ModifierGroupMapper.java
+++ backend/smart_restaurant/src/main/java/com/smart_restaurant/demo/mapper/ModifierGroupMapper.java
@@ -1,13 +1,16 @@
 package com.smart_restaurant.demo.mapper;
 
 import com.smart_restaurant.demo.dto.Request.ModifierGroupRequest;
+import com.smart_restaurant.demo.dto.Request.UpdateModifierGroupRequest;
 import com.smart_restaurant.demo.dto.Response.ModifierGroupResponse;
 import com.smart_restaurant.demo.entity.ModifierGroup;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ModifierGroupMapper {
 
     ModifierGroup toModifierGroup(ModifierGroupRequest modifierGroupRequest);
     ModifierGroupResponse toModifierGroupResponse(ModifierGroup modifierGroup);
+    void updateModifierGroup(UpdateModifierGroupRequest request, @MappingTarget ModifierGroup modifierGroup);
 }
